47f3aa09e1fd2560cb654af2643d66db30fba4a9
Eric Bidopa <ericbidopa@gmail.com>
2024-09-06 06:03:22 +0000

    still working on the backend. testing if .env works

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRouter = require("./routes/users");
+require("dotenv").config();
+
 const app = express();
-
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3001;
 
 // Midleware
-app.use(express.json());
+app.use(bodyParser.json());
 
+// Routes
 
+app.use("/users", usersRouter); // Use the within which / is qual to /users users router
 
+// Test to make sure backend is running
+app.get("/", (req, res) => {
+  res.send("OdeoPod backend running!");
+});
 
-
-
-app.listen(PORT, ()=>console.log(`server is runnig on port ${PORT}`));
-
-
+app.listen(PORT, () => console.log(`server is runnig on port ${PORT}`));
---
+++ routes/users.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+
+
+// Route to add a new user
+
+router.post("/", async (req, res) => {
+  const {
+    userId,
+    userUsername,
+    userChannelName,
+    userChannelDescription,
+    userProfileImgUrl,
+    userChannelCoverImgUrl,
+    userWalletAddress,
+    userSubscriptions,
+    userNumberOfSubscribers,
+  } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users (
+            userId, userUsername, userChannelName, userChannelDescription,
+            userProfileImgUrl, userChannelCoverImgUrl, userWalletAddress,
+            userSubscriptions, userNumberOfSubscribers
+          ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+          RETURNING *`,
+      [
+        userId,
+        userUsername,
+        userChannelName,
+        userChannelDescription,
+        userProfileImgUrl,
+        userChannelCoverImgUrl,
+        userWalletAddress,
+        userSubscriptions,
+        userNumberOfSubscribers,
+      ]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Export the router to be used in index.js
+module.exports = router;
